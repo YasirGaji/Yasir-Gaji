3af34b956604504336c3b3e9586afae9722b0239
Yasir Gaji <hellopleasantcodes@gmail.com>
2021-03-22 22:44:11 +0100

    done withn responsive header(navbar)

--- resources/js/script.js
+++ resources/js/script.js
@@ -19,4 +19,45 @@ const navlink = document.querySelectorAll('nav__link');
 
 function linkAction() {
     const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
 }
+
+navlink.forEach(n => n.addEventListener('click', linkAction));
+
+// SCROLL SECTION ACTIVE LINKS
+const sections = document.querySelectorAll('section[id]') 
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectonHeight = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectonHeight) {
+            document.querySelector(`.nav__menu a[href*=' + sectionId + ']`).classList.add('active-link')
+        } else {
+            document.querySelector(`.nav__menu a[href*=' + sectionId + ']`).classList.remove('active-link')
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+// CHANGE BACKGROUND HEADER
+
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+// SHOW SCROLL TOP
+
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollHeader)
