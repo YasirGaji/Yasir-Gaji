7db8d656c1691f1eb9ecab05ebd166addae039c7
Yasir Gaji <hellopleasantcodes@gmail.com>
2021-04-04 09:48:33 +0100

    bug on form status

--- resources/js/script.js
+++ resources/js/script.js
@@ -111,6 +111,28 @@ const mySwiper = new Swiper('.testimonial__container', {
 
 
 //SUBMISSION BEHAVIOUR
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
+
 
 //GSAP ANIMATION
 gsap.from('.home__img', {opacity: 0, duration: 2, delay:.5, x:60})
