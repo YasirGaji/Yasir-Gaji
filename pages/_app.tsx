87cc8abc56b7c33e5d4fafd1ade89afadb0bf6bb
Yasir Gaji <gajiyasir@gmail.com>
2023-02-18 04:38:42 +0100

    integrated the theme with the _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components'
 import { AppProps } from 'next/app'
+import { Theme } from '../libs'
 
 const Website = ({Component, pageProps, router} : AppProps) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={Theme}>
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
