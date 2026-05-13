4d0848ae791ba2c0f3a2b4a538b503f43aaa573e
Yasir Gaji <hellopleasantcodes@gmail.com>
2026-05-13 17:22:09 +0100

    chore: add theme tokens + Biome lint setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,10 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
